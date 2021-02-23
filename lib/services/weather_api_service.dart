c856f02b3f654bdbe6132262d65dc54250319aa9
prajwal4518 <prajwalpadal17@gmail.com>
2021-02-23 14:22:31 +0530

    Weather Card updated

--- lib/services/weather_api_service.dart
+++ lib/services/weather_api_service.dart
@@ -5,43 +5,16 @@ import 'dart:convert';
 String url =
     'http://api.openweathermap.org/data/2.5/weather?q=pune&units=metric&appid=db66c78a5b437d17537bf3faf88e4418';
 
-// class WeatherNetworkService {
-//   Future<WeatherModel> fetchData() async {
-//
-//     http.Response data = await http.get(url);
-//
-//     if (data.statusCode == 200) {
-//       Map weatherData = jsonDecode(data.body);
-//       print(WeatherModel.fromJson(weatherData));
-//
-//       return WeatherModel.fromJson(weatherData);
-//     } else {
-//
-//       throw Exception('Failed to load album');
-//     }
-//
-//
-//   }
-//
-// }
-
 class WeatherAPIService {
-
-
-
   Future<WeatherModel> fetchData() async {
     http.Response data = await http.get(url);
-
 
     if (data.statusCode == 200) {
       Map weatherData = jsonDecode(data.body);
 
       return WeatherModel.fromJson(weatherData);
     } else {
-      throw Exception('Failed to load album');
+      throw Exception("Unable to Fetch the Data");
     }
   }
-
-
-
 }
--- lib/widgets/weather_card.dart
+++ lib/widgets/weather_card.dart
@@ -3,13 +3,7 @@ import 'dart:convert';
 import 'package:crop_planning_techm/Models/weather_model.dart';
 import 'package:crop_planning_techm/services/weather_api_service.dart';
 
-
-
 import 'package:flutter/material.dart';
-import 'package:charcode/ascii.dart';
-
-import 'package:charcode/html_entity.dart' show $deg;
-
 
 class WeatherCard extends StatefulWidget {
   @override
@@ -19,13 +13,8 @@ class WeatherCard extends StatefulWidget {
 class _WeatherCardState extends State<WeatherCard> {
   final WeatherAPIService weatherData = WeatherAPIService();
 
-  // var degree = ascii.decode([0x00B0]);
-
-
-
   @override
   Widget build(BuildContext context) {
-    // print(degree.runtimeType);
     return FutureBuilder<WeatherModel>(
       future: weatherData.fetchData(),
       builder: (context, snapshot) {
@@ -35,8 +24,8 @@ class _WeatherCardState extends State<WeatherCard> {
             padding: const EdgeInsets.all(8.0),
             child: Card(
               shape: RoundedRectangleBorder(
-                   borderRadius: BorderRadius.circular(15.0),
-                  ),
+                borderRadius: BorderRadius.circular(15.0),
+              ),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.start,
                 children: [
@@ -62,18 +51,18 @@ class _WeatherCardState extends State<WeatherCard> {
                           crossAxisAlignment: CrossAxisAlignment.start,
                           children: [
                             Text(
-                              "Sunny",
+                              "${snapshot.data.weather[0].description}",
                               style: Theme.of(context).textTheme.headline4,
                             ),
                             Text(
-                              "29℃",
+                              "${snapshot.data.main.temp}℃",
                               style: Theme.of(context)
                                   .textTheme
                                   .headline3
                                   .copyWith(fontSize: 40),
                             ),
                             Text(
-                              "Day 20℃ • Night 16℃",
+                              "Day ${snapshot.data.main.temp_max.toInt()}℃ • Night ${snapshot.data.main.temp_min.toInt()}℃",
                               style: Theme.of(context).textTheme.headline4,
                             ),
                           ],
@@ -90,7 +79,7 @@ class _WeatherCardState extends State<WeatherCard> {
                                 ),
                               ),
                               Text(
-                                "Wind 14 kmph",
+                                "Wind ${snapshot.data.wind.speed} kmph",
                                 style: Theme.of(context).textTheme.headline4,
                               ),
                             ])
@@ -102,28 +91,25 @@ class _WeatherCardState extends State<WeatherCard> {
                   ),
                   Container(
                     padding: EdgeInsets.symmetric(horizontal: 8),
-                    constraints: BoxConstraints(
-                      maxHeight: 130,
-                      minHeight: 120
-                    ),
+                    constraints: BoxConstraints(maxHeight: 130, minHeight: 120),
                     child: ListView(
                       shrinkWrap: true,
                       scrollDirection: Axis.horizontal,
                       children: <Widget>[
-                        dailyTempWidget(context, "Mon",
-                            Icons.wb_sunny_rounded, "18", "27"),
-                        dailyTempWidget(context, "Tue",
-                           Icons.wb_cloudy, "28", "27"),
-                        dailyTempWidget(context, "Wed",
-                            Icons.wb_shade, "9", "28"),
-                        dailyTempWidget(context, "Thu",
-                            Icons.wb_twighlight, "10", "37"),
-                        dailyTempWidget(context, "Fri",
-                            Icons.wb_sunny_rounded, "16", "27"),
-                        dailyTempWidget(context, "Sat",
-                            Icons.wb_sunny_rounded, "23", "21"),
-                        dailyTempWidget(context, "Sun",
-                            Icons.wb_sunny_rounded, "18", "27"),
+                        dailyTempWidget(
+                            context, "Mon", Icons.wb_sunny_rounded, "18", "27"),
+                        dailyTempWidget(
+                            context, "Tue", Icons.wb_cloudy, "28", "27"),
+                        dailyTempWidget(
+                            context, "Wed", Icons.wb_shade, "9", "28"),
+                        dailyTempWidget(
+                            context, "Thu", Icons.wb_twighlight, "10", "37"),
+                        dailyTempWidget(
+                            context, "Fri", Icons.wb_sunny_rounded, "16", "27"),
+                        dailyTempWidget(
+                            context, "Sat", Icons.wb_sunny_rounded, "23", "21"),
+                        dailyTempWidget(
+                            context, "Sun", Icons.wb_sunny_rounded, "18", "27"),
                       ],
                     ),
                   )
@@ -131,88 +117,6 @@ class _WeatherCardState extends State<WeatherCard> {
               ),
             ),
           );
-
-          // return Card(
-          //   margin: EdgeInsets.symmetric(horizontal: 16),
-          //   // color: Colors.pink,
-          //   shape: RoundedRectangleBorder(
-          //     borderRadius: BorderRadius.circular(15.0),
-          //   ),
-          //   child: Padding(
-          //     padding: const EdgeInsets.all(8.0),
-          //     child: Column(
-          //       children: [
-          //         Row(
-          //           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          //           children: [
-          //             Container(
-          //               child: Row(
-          //                 children: [
-          //                   Text('Tuseday 28',style: Theme.of(context)
-          //                   .textTheme
-          //                   .headline4),
-          //                   IconButton(
-          //                       icon: Icon(
-          //                         Icons.arrow_drop_down_circle_outlined,
-          //                       ),
-          //                       onPressed: null)
-          //                 ],
-          //               ),
-          //             ),
-          //             Text("${snapshot.data.weather[0].description}",style: Theme.of(context)
-          //                 .textTheme
-          //                 .headline4),
-          //           ],
-          //         ),
-          //         Divider(
-          //           thickness: 1,
-          //         ),
-          //         Row(
-          //           mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          //           children: [
-          //             Text(
-          //               '${snapshot.data.main.temp}',
-          //               style: TextStyle(
-          //                 fontSize: 64,
-          //               ),
-          //             ),
-          //             Icon(
-          //               Icons.wb_sunny,
-          //               size: 78,
-          //             ),
-          //           ],
-          //         ),
-          //         Padding(
-          //           padding: const EdgeInsets.only(top:4),
-          //           child: Row(
-          //             mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          //             children: [
-          //               Text('${snapshot.data.main.temp_min}'
-          //                   '/'
-          //                   '${snapshot.data.main.temp_max}',style: Theme.of(context)
-          //                   .textTheme
-          //                   .headline4),
-          //               Text('Wind ${snapshot.data.wind.speed} kmph',style: Theme.of(context)
-          //                   .textTheme
-          //                   .headline4),
-          //             ],
-          //           ),
-          //         ),
-          //       ],
-          //     ),
-          //   ),
-          // );
-
-          // return Column(
-          //   children: [
-          //     Text("${snapshot.data.main.temp}"),
-          //     Text("${snapshot.data.main.temp_max}"),
-          //     Text("${snapshot.data.main.temp_min}"),
-          //     Text("${snapshot.data.main.humidity}"),
-          //     Text("${snapshot.data.weather[0].main}"),
-          //     Text("${snapshot.data.weather[0].description}"),
-          //   ],
-          // );
         } else if (snapshot.hasError) {
           return Text("${snapshot.error}");
         }
@@ -228,26 +132,34 @@ Widget dailyTempWidget(BuildContext context, String day, IconData weatherIcon,
     String minTemp, String maxTemp) {
   return Expanded(
     child: Container(
-      padding: const EdgeInsets.symmetric(horizontal:8.0,),
-      decoration: BoxDecoration(
-      // color: Theme.of(context).scaffoldBackgroundColor,
-      borderRadius: BorderRadius.circular(30),),
-        child: Column(
-          children: [
-            Text(
-              "${day}",
-              style: Theme.of(context).textTheme.headline4,
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8,),
-              child: Icon(weatherIcon, size: 40,),
-            ),
-            Text("${minTemp}/${maxTemp}℃", style: Theme.of(context).textTheme.headline3.copyWith(
-              fontSize: 14
-            ),)
-          ],
-        ),
+      padding: const EdgeInsets.symmetric(
+        horizontal: 8.0,
       ),
+      decoration: BoxDecoration(
+        // color: Theme.of(context).scaffoldBackgroundColor,
+        borderRadius: BorderRadius.circular(30),
+      ),
+      child: Column(
+        children: [
+          Text(
+            "${day}",
+            style: Theme.of(context).textTheme.headline4,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              vertical: 8,
+            ),
+            child: Icon(
+              weatherIcon,
+              size: 40,
+            ),
+          ),
+          Text(
+            "${minTemp}/${maxTemp}℃",
+            style: Theme.of(context).textTheme.headline3.copyWith(fontSize: 14),
+          )
+        ],
+      ),
+    ),
   );
-
 }
